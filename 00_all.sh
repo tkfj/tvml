5e818cb1ceec486b8d7ba2f12a34dd6dc0b985fa
tkfj <tk@fjworks.jp>
2026-06-22 09:37:57 +0000

    起動スクリプト追加

---
+++ 00_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+docker compose -f ${SCRIPT_DIR}/docker-compose.yml run --rm app python src/test.py
+docker compose -f ${SCRIPT_DIR}/docker-compose.yml run --rm app python src/main.py
